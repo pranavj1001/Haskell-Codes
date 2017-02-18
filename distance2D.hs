e092470992aa0fbb627efe23132a028d854987df
pranavj1001 <pranavj1001@gmail.com>
2017-02-18 23:58:53 +0530

    update the distance2D
    now the function can check distance between two points on a 3D plane
    also

--- distance2D.hs
+++ distance2D.hs
@@ -5,3 +5,8 @@ type Point2D = (Float, Float)
 
 distance :: Point2D -> Point2D -> Float
 distance (x1, y1) (x2, y2) = sqrt(sqr(x2 - x1) + sqr(y2 - y1))
+
+type Point3D = (Float, Float, Float)
+
+distance :: Point3D -> Point3D -> Point3D
+distance (x1, y1, z1) (x2, y2, z2) = sqrt(sqr(x2 - x1) + sqr(y2 - y1) + sqr(z2-z1))
