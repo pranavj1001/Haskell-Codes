f4744cfa4f23349b1ae091a90cbb21c2d3397be6
Pranav Jain <pranavj1001@gmail.com>
2017-04-26 22:00:36 +0530

    add complexNumberData.hs

---
+++ complexNumberData.hs
@@ -0,0 +1,12 @@
+import Data.Complex
+
+number = 3 :+ 4
+
+main = do
+    print number
+    print $ realPart number
+    print $ imagPart number
+    print $ polar number
+    print $ magnitude number
+    print $ phase number
+    print $ conjugate number
