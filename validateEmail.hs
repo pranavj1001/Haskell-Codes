494b51e40021f093c53558c3bc2c9c7290355811
Pranav Jain <pranavj1001@gmail.com>
2017-05-12 23:37:14 +0530

    add validateEmail.hs

---
+++ validateEmail.hs
@@ -0,0 +1,13 @@
+import Text.Email.Validate
+
+email = "eriksalaj@gmail.com"
+
+main = do
+    print $ isValid email
+    print $ validate email
+    print $ emailAddress email
+    print $ canonicalizeEmail email
+
+    let Just address = emailAddress email
+    print $ localPart address
+    print $ domainPart address
